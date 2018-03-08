e1f889b88929a39f04a99532c1e257ae4480bae7
Javier Contreras <contranton@gmail.com>
2018-03-08 15:59:16 -0300

    Introduced __str__ and renamed Square to Rectangle

--- Actividades/AC00/geometry.py
+++ Actividades/AC00/geometry.py
@@ -19,22 +19,29 @@ class Circle (Shape):
     def get_area(self):
         return self.radius**2*pi
 
-    def get_perimeter (self):
+    def get_perimeter(self):
         return self.radius*pi*2
 
+    def __str__(self):
+        return "Circle of radius %d at position (%d, %d)" %\
+            (self.radius, *self.position)
 
-class Square (Shape):
-    def __init__ (self, position, width, height):
+class Rectangle (Shape):
+    def __init__(self, position, width, height):
         super ().__init__(position)
         self.width = width
         self.height = height
 
-    def get_area (self):
+    def get_area(self):
         return self.width*self.height
 
-    def get_perimeter (self):
+    def get_perimeter(self):
         return 2*self.width + 2*self.height
 
-    def is_square (self):
+    def is_square(self):
         return self.width == self.height
+
+    def __str__(self):
+        return "Square with width %d and height %d" %\
+            (self.width, self.height)
         
