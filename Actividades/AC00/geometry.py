d4d13ddb993623da02d322ca81f1c3952a23341b
Javier Contreras <contranton@gmail.com>
2018-03-08 15:51:54 -0300

    Implemented Circle and its area/perimeter methods

--- Actividades/AC00/geometry.py
+++ Actividades/AC00/geometry.py
@@ -10,4 +10,15 @@ class Shape:
     def get_perimeter(self):
         raise NotImplementedError()
 
+    
+class Circle (Shape):
+    def __init__(self, position, radius):
+        super(self).init(position)
+        self.radius = radius
+
+    def get_area(self):
+        return self.radius**2*pi
+
+    def get_perimeter (self):
+        return self.radius*pi*2
         
