26b28c0f567eb8d2b64b6580d078a298d0646f19
Javier Contreras <contranton@gmail.com>
2018-03-22 16:08:38 -0300

    AC02 First turn-in

--- Actividades/AC02/main.py
+++ Actividades/AC02/main.py
@@ -1,3 +1,6 @@
+from collections import namedtuple, deque
+
+
 class PAWControl:
 
     MAX_PASSENGERS = 5
@@ -13,39 +16,53 @@ class PAWControl:
         :param path: path al archivo de clientes
         :return: estructura de datos con la info de clientes
         """
+
+        clients = {}
+        
         with open(path, "r", encoding="utf-8") as file:
             for line in file:
                 name, passport = line.strip("\n").split(",")
                 # COMPLETAR PROCESAMIENTO DESDE AQUÍ
-
+                clients[passport] = name
                 # HASTA AQUÍ
         # PONER RETORNO
+        return clients
 
     def _read_flights(self, path):
         """
         :param path: path al archivo de vuelos
         :return: estructura de datos con los vuelos
         """
+
+        flights = {}
+        flight = namedtuple("flight", "passengers origin destination")
+        
         with open(path, "r", encoding="utf-8") as file:
             for line in file:
                 fid, origin, destination = line.strip("\n").split(",")
                 # COMPLETAR PROCESAMIENTO DESDE AQUÍ
-
+                flights[fid] = flight({}, origin, destination)
                 # HASTA AQUÍ
         # PONER RETORNO
+        return flights
 
     def _read_bookings(self, path):
         """
         :param path: path al archivo de reservas acumuladas
         :return: estructura de datos con las reservas por orden de llegada
         """
+
+        booking = deque()
+        entry = namedtuple("booking", "fid passport seat")
+        
         with open(path, "r", encoding="utf-8") as file:
             for line in file:
                 passport, fid, seat = line.strip("\n").split(",")
                 # COMPLETAR PROCESAMIENTO DESDE AQUÍ
-
+                booking.appendleft(entry(fid, passport, seat))
                 # HASTA AQUÍ
         # PONER RETORNO
+        return booking
 
     def recovery(self, clients, booking, flights):
         """
@@ -58,7 +75,9 @@ class PAWControl:
         en las variables clients, bookings, rejected y flights.
         """
         # COMPLETAR BORRANDO pass
-        pass
+        self.clients = self._read_clients(clients)
+        self.bookings = self._read_bookings(booking)
+        self.flights = self._read_flights(flights)
 
     def assign_seats(self):
         """
@@ -67,11 +86,21 @@ class PAWControl:
         el registro de asientos de los aviones para las reservas
         aceptadas y guardar en self.rejected las reservas rechazadas
         """
+        self.rejected = list()
         # COMPLETAR BORRANDO pass
-        pass
+        while len(self.bookings) != 0:
+            booking_attempt = self.bookings.pop()
+            flight = self.flights[booking_attempt.fid]
+            seat = booking_attempt.seat
+            if seat not in flight.passengers:
+                    flight.passengers[seat] = booking_attempt.passport
+            else:
+                # Seat has been occupied
+                self.rejected.append(booking_attempt)
 
     def passenger_list(self, flight_id):
-        '''Recibe el id de un vuelo y RETORNA una lista con todas las tuplas de la forma (seat_number, passport, client_name)'''
+        '''Recibe el id de un vuelo y RETORNA una lista con
+        todas las tuplas de la forma (seat_number, passport, client_name)'''
         flight_indeed = self.flights[flight_id]
         passenger_list = list()
         # Iteramos en el orden pedido
@@ -91,11 +120,17 @@ class PAWControl:
 
     def passengers_to_destination(self, destination):
         # COMPLETAR BORRANDO pass
-        pass
+        # No me salio como set comprehension :c
+        passengers = set()
+        for flight in self.flights.values():
+            if flight.destination == destination:
+                passengers = passengers.union({*flight.passengers.values()})
+        return list(passengers)
 
     def rejected_bookings(self):
         # COMPLETAR BORRANDO pass
-        pass
+        return [(r.passport, self.clients[r.passport], r.fid, r.seat)
+                for r in self.rejected]
 
 
 if __name__ == '__main__':
