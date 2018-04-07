dba3b8f08291ede7c8b0512ecbad6cd39eb7b40e
Javier Contreras <contranton@gmail.com>
2018-04-07 12:27:28 -0300

    Make race objects singletons

--- Tareas/T01/razas.py
+++ Tareas/T01/razas.py
@@ -6,6 +6,8 @@ Since a race encodes the properties of the actions of multiple planets, it
 makes no sense to instatiate them. Thus, they remain abstract classes!
 """
 
+RACE_INSTANCES = set()
+Cost = namedtuple("Cost", "mins deut")
 
 class Raza(metaclass=ABCMeta):
     """Defines a race's properties, to be used in individual planets
@@ -16,17 +18,47 @@ class Raza(metaclass=ABCMeta):
     """
 
     name = ""
-    
-    Cost = namedtuple("Cost", "mins deut")
 
     has_mago = False
 
+    warcry = "Test"
+
+    def __init__(self):
+        "docstring"
+        if type(self) not in RACE_INSTANCES:
+            RACE_INSTANCES.add(type(self))
+
+            s = ("Wubba Lubba Dub Dub, hemos logrado conquistar un" +
+                 " nuevo planeta\n")
+            self.warcry = s + self.__class__.warcry
+        else:
+            del self
+            raise Exception("Instantiated multiple race classes" +
+                            ". We're a singleton bruh.")
+
     @staticmethod
+<<<<<<< Updated upstream
     @abstractmethod
     def habilidad():
         """Special race ability, used in battles"""
         pass
 
+=======
+    def habilidad(entity, enemy_entity):
+        """Special race ability, used in battles"""
+        pass
+
+    @property
+    def warcry(self):
+        
+        s += self._warcry
+        return s
+
+    @warcry.setter
+    def warcry(self, value):
+        self._warcry = value
+
+>>>>>>> Stashed changes
 
 class MaestroRaza(Raza):
 
@@ -34,21 +66,23 @@ class MaestroRaza(Raza):
 
     max_pop = 100
 
-    costo_soldado = Raza.Cost(mins=200, deut=300)
+    costo_soldado = Cost(mins=200, deut=300)
     rango_atq_soldado = (60, 80)
     rango_vid_soldado = (200, 250)
 
     has_mago = True
-    costo_mago = Raza.Cost(mins=300, deut=400)
+    costo_mago = Cost(mins=300, deut=400)
     rango_atq_mago = (80, 120)
     rango_vid_mago = (150, 200)
 
-    warcry = "¡Nuestro conocimiento nos ha\notrogado una victoria más!"
+    warcry = ("¡Nuestro conocimiento nos ha "
+              "otorgado una victoria más!")
 
     @staticmethod
     @abstractmethod
     def habilidad():
         pass
+>>>>>>> Stashed changes
 
 
 class AprendizRaza(Raza):
@@ -57,7 +91,7 @@ class AprendizRaza(Raza):
     
     max_pop = 100
 
-    costo_soldado = Raza.Cost(mins=300, deut=400)
+    costo_soldado = Cost(mins=300, deut=400)
     rango_atq_soldado = (30, 60)
     rango_vid_soldado = (600, 700)
 
@@ -87,3 +121,6 @@ class AsesinoRaza(Raza):
     @abstractmethod
     def habilidad():
         print("Ugh")
+maestro = MaestroRaza()
+aprendiz = AprendizRaza()
+asesino = AsesinoRaza()
